e5ed37526cb5f4e0c83c74597f496923528446ee
AnnaSidiakina <fruktik@gmail.com>
2022-08-01 20:41:42 +0200

    hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const throttle = require('lodash.throttle');
+
+const onPlay = function (timeupdate) {
+  localStorage.setItem('videoplayer-current-time', timeupdate.seconds);
+
+  console.log(timeupdate.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const setTimeToPlay = localStorage.getItem('videoplayer-current-time');
+
+if (setTimeToPlay) {
+  player.setCurrentTime(setTimeToPlay);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  //   console.log(formData);
+  localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
+}
+
+populateFormData();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+  formData = {};
+
+  event.target.reset();
+
+  localStorage.removeItem('STORAGE_KEY');
+}
+
+function populateFormData() {
+  const savedMessage = JSON.parse(localStorage.getItem('STORAGE_KEY'));
+  if (savedMessage) {
+    refs.email.value = savedMessage.email;
+    refs.textarea.value = savedMessage.message;
+  }
+}
