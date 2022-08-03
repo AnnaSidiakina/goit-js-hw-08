15a963764292a594caad8a930b8c1325016e906d
AnnaSidiakina <fruktik@gmail.com>
2022-08-03 20:31:03 +0200

    fix bugs

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,10 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-let formData = {};
+let formData = {
+  email: '',
+  message: '',
+};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
@@ -14,7 +17,7 @@ refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormInput(event) {
   formData[event.target.name] = event.target.value;
-  //   console.log(formData);
+
   localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
 }
 
@@ -22,8 +25,22 @@ populateFormData();
 
 function onFormSubmit(event) {
   event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.target;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please, fill all the fields!');
+  }
+  // formData = {
+  //   email: email.value,
+  //   message: message.value,
+  // };
+  // formData[event.target.name] = event.target.value;
   console.log(formData);
-  formData = {};
+
+  formData.email = '';
+  formData.message = '';
 
   event.target.reset();
 
@@ -32,8 +49,9 @@ function onFormSubmit(event) {
 
 function populateFormData() {
   const savedMessage = JSON.parse(localStorage.getItem('STORAGE_KEY'));
-  if (savedMessage) {
-    refs.email.value = savedMessage.email;
-    refs.textarea.value = savedMessage.message;
+  if (savedMessage === null) {
+    return;
   }
+  refs.email.value = savedMessage.email || '';
+  refs.textarea.value = savedMessage.message || '';
 }
